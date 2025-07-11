89c576b217fe4aff949e5016ea1433bf0ef5199d
CodeSrushti-3105 <srushtiahire3105@gmail.com>
2025-07-11 15:15:30 +0530

    requirement commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 # ✅ CORS for React
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
